2bd00be19e22ef73ca3f8adaab5e4372d0ed4784
Giovanny Hernandez Arbelaez <46624723+giovanni104@users.noreply.github.com>
2023-10-30 18:59:37 -0500

    avances proyecto 2 hateoas y mysql

---
+++ apisRestUdemy/proyecto02/api-rest-cuentas/src/main/java/com/api/hateoas/repository/CuentaRepository.java
@@ -0,0 +1,11 @@
+package com.api.hateoas.repository;
+
+import com.api.hateoas.model.Cuenta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CuentaRepository extends JpaRepository<Cuenta,Integer> {
+}
