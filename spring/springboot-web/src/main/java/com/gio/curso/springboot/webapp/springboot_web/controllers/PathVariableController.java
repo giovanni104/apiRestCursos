45d99aabc69f68bef4035493c9eaee1d1c315921
Giovanny Hernandez Arbelaez <giovanni104@hotmail.com>
2024-08-18 19:43:14 -0500

    avance del curso

--- spring/springboot-web/src/main/java/com/gio/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ spring/springboot-web/src/main/java/com/gio/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -2,7 +2,11 @@ package com.gio.curso.springboot.webapp.springboot_web.controllers;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,12 +17,39 @@ import org.springframework.web.bind.annotation.RestController;
 import com.gio.curso.springboot.webapp.springboot_web.models.User;
 import com.gio.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
 
- 
-
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
+
+    @Value("${config.username}")
+    private String username;
+
+    @Value("${config.listOfValues}")
+    private List<String> listOfValues;
+    @Value("#{'${config.listOfValues}'.toUpperCase().split(',')}")
+    private List<String> valueList;
+
+    @Value("#{'${config.listOfValues}'.toUpperCase()}")
+    private String valueString;
+
+    @Value("${config.code}")
+    private Integer code;
+    @Value("#{${config.valuesMap}}")
+    private Map<String, Object> valuesMap;
+
+    @Value("#{${config.valuesMap}.product}")
+    private String product;
     
+    @Value("#{${config.valuesMap}.price}")
+    private Long price;
+
+    @Autowired
+    private Environment environment;
+
+
+    //@Value("${config.message}")
+    //private String message;
+
     @GetMapping("/baz/{message}")
     public ParamDto baz(@PathVariable String message) {
 
@@ -26,10 +57,10 @@ public class PathVariableController {
         param.setMessage(message);
         return param;
     }
-    
+
     @GetMapping("/mix/{product}/{id}")
     public Map<String, Object> mixPathVar(@PathVariable String product, @PathVariable Long id) {
-        
+
         Map<String, Object> json = new HashMap<>();
         json.put("product", product);
         json.put("id", id);
@@ -44,7 +75,22 @@ public class PathVariableController {
         return user;
     }
 
-
-
+    @GetMapping("/values")
+    public Map<String, Object> values(@Value("${config.message}") String message) {
+        Long code2 = environment.getProperty("config.code", Long.class);
+        Map<String, Object> json = new HashMap<>();
+        json.put("username", username);
+        json.put("code", code);
+        json.put("code2", code2);
+        json.put("message", message);
+        json.put("message2", environment.getProperty("config.message"));
+        json.put("listOfValues", listOfValues);
+        json.put("valueString", valueString);
+        json.put("valueList", valueList);
+        json.put("valueMap", valuesMap);
+        json.put("product", product);
+        json.put("price", price);
+        return json;
+    }
 
 }
