fc860e81f969e308d22315da739372cc6804ca46
Giovanny Hernandez Arbelaez <46624723+giovanni104@users.noreply.github.com>
2024-08-26 22:48:42 -0500

    avances curso

---
+++ spring/springboot-web/src/main/java/com/gio/curso/springboot/webapp/springboot_web/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.gio.curso.springboot.webapp.springboot_web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    
+    @GetMapping({"", "/", "/home"})
+    public String home() {
+
+        //return "redirect:/list";
+          return "forward:/list";
+    }
+}
+
