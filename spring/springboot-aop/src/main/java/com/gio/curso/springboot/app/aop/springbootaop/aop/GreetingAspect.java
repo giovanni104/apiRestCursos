dbfdecb2f5e5fca8454128afdaa82f1411af0221
Giovanny Hernandez Arbelaez <46624723+giovanni104@users.noreply.github.com>
2024-11-21 09:05:41 -0500

    avance de aspectos

--- spring/springboot-aop/src/main/java/com/gio/curso/springboot/app/aop/springbootaop/aop/GreetingAspect.java
+++ spring/springboot-aop/src/main/java/com/gio/curso/springboot/app/aop/springbootaop/aop/GreetingAspect.java
@@ -3,15 +3,18 @@ package com.gio.curso.springboot.app.aop.springbootaop.aop;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-
+@Order(2)
 @Aspect
 @Component
 public class GreetingAspect {
@@ -48,4 +51,28 @@ public class GreetingAspect {
         String args = Arrays.toString(joinPoint.getArgs());
         logger.info("Despues lanzar la excepcion: " + method + " con los argumentos " + args);
     }
+    
+
+   @Around("execution(* com.gio.curso.springboot.app.aop.springbootaop.services.GreetingService.*(..))")
+    public Object loggerAround(ProceedingJoinPoint joinPoint) throws Throwable{
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+
+        Object result = null;
+        try {
+            logger.info("El metodo " + method + "() con los parametros " + args);
+            result = joinPoint.proceed();
+            logger.info("El metodo " + method + "() retorna el resultado: " + result);
+            return result;
+        } catch (Throwable e) {
+            logger.error("Error en la llamada del metodo " + method + "()");
+            throw e;
+        }
+
+    }
+
+
+
+
+
 }
