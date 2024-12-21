c952b1a2517f8cc0be5e3657b6acc16a525d9887
Giovanny Hernandez Arbelaez <46624723+giovanni104@users.noreply.github.com>
2024-12-21 14:21:56 -0500

    anaces jpa

---
+++ spring/springboot-jpa/src/main/java/com/gio/curso/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.gio.curso.springboot.jpa.springboot_jpa.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.gio.curso.springboot.jpa.springboot_jpa.entities.Person;
+
+public interface PersonRepository extends CrudRepository<Person, Long> {
+    
+}
